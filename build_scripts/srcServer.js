7836432358f1f5990542ad83006262b7dccb2479
omargon <omargon@gmail.com>
2016-12-13 20:29:42 -0500

    Add express server script and index.html

---
+++ build_scripts/srcServer.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var path = require('path')
+var open = require('open')
+
+var port = 3000
+var app = express()
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../src/index.html'))
+})
+
+app.listen(port, function (err) {
+  console.log('Example app listening on port ' + port + '!')
+  open('http://localhost:' + port)
+})
